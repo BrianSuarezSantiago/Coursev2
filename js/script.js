5c65e1b0730d1d550f72708f3b12e505f0aa762a
Brian <briansuarezsantiago@gmail.com>
2022-08-09 13:08:21 +0100

    v 2.0

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+    let a = document.getElementById("select-attack");
+    a.style.display = "none";
+    //a.hidden = true;
+
+    let b = document.getElementById("reset-game");
+    b.style.display = "none";
+})
